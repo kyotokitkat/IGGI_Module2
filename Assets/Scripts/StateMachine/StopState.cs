e227f06470b48d4dba8f0a9eadaddb5db88621fc
Zoe <zoshe001@gold.ac.uk>
2016-02-29 00:26:43 +0000

    // tidy up

--- Assets/Scripts/StateMachine/StopState.cs
+++ Assets/Scripts/StateMachine/StopState.cs
@@ -13,75 +13,18 @@ public class StopState : IState
 
     bool paused = false;
 
-    //private void Start()
-    //   {
-    //      Time.timeScale = 1f;
-    // }
-
     public void UpdateState()
     {
         
             paused = togglePause();
-        /* if (paused)
-        {
-            
-            Time.timeScale = 0f;
-            return
-            Debug.Log("pause");
-        }
-
-        else
-        {
-            paused = true;
-           // Time.timeScale = 1f;
-            ToInspirationState();
-        } */
-        // if (Input.GetKeyDown(KeyCode.P)) //check if Escape key/Back key is pressed
-
-        /* if (paused)
-             paused = false;  //unpause the game if already paused
-         else
-              paused = true;  //pause the game if not paused
-
-
-          if (paused)
-
-          {
-              Time.timeScale = 0f;
-              Debug.Log("pause");
-          }
-          //set the timeScale to 0 so that all the procedings are halted
-
-          else
-          {
-             ToInspirationState(); */
-
-        /*}
-
-         if (Time.timeScale == 1f)
-         {
-             Time.timeScale = 0f;
-         Debug.Log ("Pause");
-         }
-
-         else
-         {
-             Time.timeScale = 1f;
-             ToInspirationState();
-         Debug.Log ("Unpause");
-         } */
-
-        //}
+  
     }
-
-    //its both pausing and unpausing
+  
     //also interaction with other bots? conflicts?
 
     public void ToInspirationState()
     {
 
-        //Time.timeScale = 1f;     //set it back to 1 on unpausing the game
-        // Debug.Log("unpause");
         bot.currentState = bot.inspirationState;
         Debug.Log("going to inspiration state");
     }
